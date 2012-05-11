f5e6c7469fbbe85ec08c25558178199e443864ce
timkrins <timkrins@gmail.com>
2012-05-11 12:14:18 -0700

    Added makeCylinder etc routines

--- delta.c
+++ delta.c
@@ -21,6 +21,7 @@ const int fs_window_width = 1366, fs_window_height = 768;
 
 #define TOP_PLAT_DIST 10.0f
 #define TOP_PLAT_THICK 3.0f
+#define TOP_CYL_WIDTH 5.0f
 
 GLubyte rasters[24] = {
    0xc0, 0x00, 0xc0, 0x00, 0xc0, 0x00, 0xc0, 0x00, 0xc0, 0x00,
@@ -34,6 +35,13 @@ float yrot, xrot;
 float speedmin  = 0.15f;
 float speedmulti = 0.05f;
 float c_red = 0.1f, c_green = 0.1f, c_blue = 0.1f;
+
+
+GLUquadric *quadSphere;
+GLUquadric *quadCylinder;
+GLdouble radius = 1.0;
+GLint slices, stacks;
+GLint nsides, rings;
 
 //define delta static vars
 
@@ -144,6 +152,9 @@ void graphics_init(void) {
   glMatrixMode(GL_MODELVIEW);
   glClearColor( 0.05f, 0.05f, 0.05f, 0.0f ); // Preset color
   makeRasterFont();
+  quadCylinder=gluNewQuadric();
+  quadSphere=gluNewQuadric();
+  //glfwSetMouseWheelCallback( wheelmoved );
 }//end-of-graphics-initialise
 
 void printString(char *s)
@@ -153,16 +164,64 @@ void printString(char *s)
    glCallLists(strlen(s), GL_UNSIGNED_BYTE, (GLubyte *) s);
    glPopAttrib ();
 }
+void makeSphereAt(float height_position, float center_radius, float angle, float sphere_radius)
+{
+  // a 'rotate and make sphere' function
+  
+  glPushMatrix();
+  glRotatef(angle,0,1,0);
+  glTranslatef(0,height_position,center_radius);
+  gluSphere(quadSphere, sphere_radius, 32, 16);
+  glPopMatrix();
+}
+
+void makeHorizontalCylinderAt(float height_position, float center_radius, float angle, float cyl_radius, float cyl_width)
+{
+  // a 'rotate and make cylinders' function
+  
+  glPushMatrix();
+  glRotatef(angle,0,1,0);
+  glTranslatef(0,height_position,center_radius);
+  glRotatef(90,0,1,0);
+  glTranslatef(0,0,0-(cyl_width/2));
+  gluCylinder(quadCylinder, cyl_radius, cyl_radius, cyl_width, 32, 16);
+  glPopMatrix();
+}
+
+void makeHorizontalCapsuleAt(float height_position, float center_radius, float angle, float cyl_radius, float cyl_width)
+{
+  // a 'rotate and make capsule' function
+  
+  glPushMatrix();
+  glRotatef(angle,0,1,0);
+  glTranslatef(0,height_position,center_radius);
+  glRotatef(90,0,1,0);
+  glTranslatef(0,0,0-(cyl_width/2));
+  gluCylinder(quadCylinder, cyl_radius, cyl_radius, cyl_width, 32, 16);
+  glPopMatrix();
+  float colors[4];
+  glGetFloatv(GL_CURRENT_COLOR, colors);
+  glColor3f(colors[0]*0.8, colors[1]*0.8, colors[2]*0.8);
+  glPushMatrix();
+  glRotatef(angle,0,1,0);
+  glTranslatef(0,height_position,center_radius);
+  glRotatef(90,0,1,0);
+  glTranslatef(0,0,0-(cyl_width/2));
+  gluSphere(quadSphere, cyl_radius*1.1, 32, 16);
+  glTranslatef(0,0,cyl_width);
+  gluSphere(quadSphere, cyl_radius*1.1, 32, 16);
+  glPopMatrix();
+}
 
 void draw_top_plat(void) {  // Draws the top platform
+glPushMatrix();
   glBegin(GL_TRIANGLES);
   {    //glColor3f(c_red, c_green, c_blue);
    glColor4f(0,0,0,0);
    glColor3f(.5f, .1f, .2f);
-   
    glVertex3f(top_v_2[0][0],top_v_2[0][1],top_v_2[0][2]); //1,2,3
-  glVertex3f(top_v_2[1][0],top_v_2[1][1],top_v_2[1][2]);
-glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]);
+   glVertex3f(top_v_2[1][0],top_v_2[1][1],top_v_2[1][2]);
+   glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]);
    glColor3f(.3f, .7f, .3f);
    glVertex3f(top_v_2[0][0],top_v_2[0][1],top_v_2[0][2]); //1,5,2
    glVertex3f(top_v_2[4][0],top_v_2[4][1],top_v_2[4][2]);
@@ -178,11 +237,11 @@ glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]);
    glColor3f(.7f, .7f, .8f);
    glVertex3f(top_v_2[1][0],top_v_2[1][1],top_v_2[1][2]); //2,3,6
    glVertex3f(top_v_2[5][0],top_v_2[5][1],top_v_2[5][2]);
-      glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]);
+   glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]);
    glColor3f(.5f, .1f, .8f);
    glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]); //3,4,6
    glVertex3f(top_v_2[5][0],top_v_2[5][1],top_v_2[5][2]);
-      glVertex3f(top_v_2[3][0],top_v_2[3][1],top_v_2[3][2]);
+   glVertex3f(top_v_2[3][0],top_v_2[3][1],top_v_2[3][2]);
    glColor3f(.5f, .7f, .3f);
    glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]); //3,4,1
    glVertex3f(top_v_2[3][0],top_v_2[3][1],top_v_2[3][2]);
@@ -193,37 +252,7 @@ glVertex3f(top_v_2[2][0],top_v_2[2][1],top_v_2[2][2]);
    glVertex3f(top_v_2[5][0],top_v_2[5][1],top_v_2[5][2]);
   }
   glEnd();
-}//end-of-draw-top-plat
-
-void draw_machine(void) {  // Draws a square with a gradient color at coordinates 0, 10
-  glBegin(GL_QUADS);
-  {
-    glColor3f(c_red, c_green, c_blue*3);
-    glVertex3f(-10, -10, -1.5);
-    glVertex3f(10, -10, -1.5);
-    glColor3f(0.1,0.9,0.9);
-    glVertex3f(10, 10, -1.5);
-    glVertex3f(-10, 10, -1.5);
-  }
-  glEnd();
-}//end-of-drawing-the-back
-
-void graphics_draw(void) {
-    glLoadIdentity();
-   	glTranslatef(0, 0, -70);  // move view back a bit 
-    glfwGetMousePos(&xMouse,&yMouse);
-    gluLookAt(
-    (double)(xMouse-(window_width/2))/20, ((double)(yMouse-(window_height/2))/20)+0, 12, // look from
-		0, -5, 0, // look here
-		0.0f, 1.0f, 0.0f); // up matrix
-		
-		glPushMatrix();
-		//draw things here
-    draw_top_plat();
-    //draw_machine();
-    //end drawing things
-    glPopMatrix();
-    float color = 0.9;
+      float color = 0.9;
     glColor3f(color, color, color);
     glRasterPos3f(top_v_2[0][0],top_v_2[0][1]+0.5,top_v_2[0][2]);
     printString("ONE");
@@ -245,6 +274,54 @@ void graphics_draw(void) {
     printString("ANGLE TWO");
     glRasterPos3f(-20,-10,-5);
     printString("ANGLE THREE");
+  glPopMatrix();
+  
+  glColor3f(.1f, .9f, .1f);
+  //makeHorizontalCylinderAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 300.0,(TOP_PLAT_THICK/1.9),TOP_CYL_WIDTH); 
+  makeHorizontalCapsuleAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 300.0,(TOP_PLAT_THICK/1.9),TOP_CYL_WIDTH);
+  //makeSphereAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 300.0,(TOP_PLAT_THICK/1.9)); 
+  
+  glColor3f(.1f, .9f, .1f);
+  makeHorizontalCylinderAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 180.0,(TOP_PLAT_THICK/1.9),TOP_CYL_WIDTH); 
+  //makeSphereAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 180.0,(TOP_PLAT_THICK/1.9)); 
+  
+  glColor3f(.1f, .1f, .9f);
+  makeHorizontalCylinderAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 60.0,(TOP_PLAT_THICK/1.9),TOP_CYL_WIDTH); 
+  //makeSphereAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 60.0,(TOP_PLAT_THICK/1.9)); 
+  
+  glColor3f(.9f, .1f, .9f);
+  //makeHorizontalCylinderAt(0-(TOP_PLAT_THICK/2),TOP_PLAT_DIST, 60.0,(TOP_PLAT_THICK/1.9),TOP_CYL_WIDTH); 
+  
+}//end-of-draw-top-plat
+
+void draw_machine(void) {  // Draws a square with a gradient color at coordinates 0, 10
+  glBegin(GL_QUADS);
+  {
+    glColor3f(c_red, c_green, c_blue*3);
+    glVertex3f(-10, -10, -1.5);
+    glVertex3f(10, -10, -1.5);
+    glColor3f(0.1,0.9,0.9);
+    glVertex3f(10, 10, -1.5);
+    glVertex3f(-10, 10, -1.5);
+  }
+  glEnd();
+}//end-of-drawing-the-back
+
+void graphics_draw(void) {
+    glLoadIdentity();
+   	glTranslatef(0, 0, (glfwGetMouseWheel()*3)-70);  // move view back a bit 
+    glfwGetMousePos(&xMouse,&yMouse);
+    gluLookAt(
+    (double)(xMouse-(window_width/2))/20, ((double)(yMouse-(window_height/2))/20)+0, 12, // look from
+		0, -5, 0, // look here
+		0.0f, 1.0f, 0.0f); // up matrix
+		
+		
+		//draw things here
+    draw_top_plat();
+    //draw_machine();
+    //end drawing things
+   
     glFlush();
     
    // glPushMatrix();
